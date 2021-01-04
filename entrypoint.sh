c68a18ec63b0a10f1d6d4cfcd333cd28ba01f7aa
Sergey Brovko <help@cyber01.ru>
2021-01-04 22:27:35 +0300

    no_server_name

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "x$SERVER_NAMES" != "x" ]; then
-    # Replace commas with spaces
-    SERVER_ALIAS="`printf '%s\n' "$SERVER_NAMES" | tr ',' ' '`"
-    sed -e "s|server_name .*|server_name $SERVER_ALIAS;|" \
-        -i /etc/nginx/conf.d/default.conf
-fi
